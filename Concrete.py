1206135585bd1d43b3ed0760eb1ac76a5446d129
sailing <176530355@qq.com>
2024-11-14 22:43:45 +0800

    yu

---
+++ Concrete.py
@@ -0,0 +1,5 @@
+import json
+import pandas as pd
+
+concrete_df=pd.read_json('./Concrete.json')
+print(concrete_df)
---
+++ Reinforce.py
@@ -0,0 +1,5 @@
+import json
+import pandas as pd
+
+reinforce_df=pd.read_json('./Reinforce.json')
+print(reinforce_df)
